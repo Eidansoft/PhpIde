1814c622302ed258b3ec191503f318625493006d
Lorente, Alejandro <eidansoft@gmail.com>
2016-01-10 02:08:31 +0100

    Added target folder to git repository

---
+++ target/phpide.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>IDE test Angular y Code Mirror</title>
+	
+	<link href="phpide-1.2.0-min.css" rel="stylesheet">
+	
+</head>
+<body ng-app="phpide" ng-controller="mainController">
+	<div class="container-fluid">
+		<div class="row">
+			<div id="phpide-title" class="col-xs-2"><h2>IDE test</h2></div>
+			<div id="phpide-buttonbar" class="col-xs-10" ng-include="'./partials/buttons.html'"/></div>
+		</div>
+		<div class="row">
+			<div id="phpide-fileTree" class="col-xs-2" ng-include="'./partials/filesTree.html'"></div>
+			<div id="phpide-editor" class="col-xs-10" ng-include="'./partials/editor.html'"/></div>
+		</div>
+	</div>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
+	<script src="phpide-1.2.0-min.js"></script>
+	<!-- script src="./lib/ui-bootstrap-tpls-0.14.3.min/ui-bootstrap-tpls-0.14.3.min.js"></script-->
+	<!-- script src="./lib/ngDialog-master/js/ngDialog.min.js"></script-->
+
+
+
+	<div ng-include="'./partials/generic/alerts.html'"></div>
+	<div ng-include="'./partials/generic/prompt.html'"></div>
+	<div ng-include="'./partials/generic/selectFolder.html'"></div>
+</body>
+</html>
